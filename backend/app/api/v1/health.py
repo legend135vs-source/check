bae843481180d517a216b1d2c497a90a028a56b3
legend135vs-source <legend.135.vs@gmail.com>
2026-08-05 00:43:23 +0300

    Use non-empty healthcheck path

--- backend/app/api/v1/health.py
+++ backend/app/api/v1/health.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/health", tags=["Health"])
 
 
-@router.get("")
 @router.get("/")
 async def healthcheck() -> dict[str, str]:
     return {"status": "ok"}
